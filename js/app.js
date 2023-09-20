99da50f7e962ffc06cebbb1c9eacf32f963e7842
Gonzalo Billia <gonzalobillia@gmail.com>
2023-09-19 22:55:24 -0300

    elimino segundo carrusel

---
+++ js/app.js
@@ -0,0 +1,5 @@
+//pop over bootstrap funcion
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+//pop over bootstrap funcion
+
